cda8172fc867399f29c7ef77438e150075818d60
Tim0n26 <146103019+Tim0n26@users.noreply.github.com>
2024-02-13 17:05:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if(optionOneId == optionTwoId) {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('You have clicked the same image!')
+      //alert('You have clicked the same image!')
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
